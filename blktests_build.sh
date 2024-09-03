e255e52f647e4bd9ad75d038f4561cab21e97747
Xiao Liang <xiliang@redhat.com>
2024-09-03 16:05:44 +0800

    added ltp_build.sh and blktests_build.sh
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

---
+++ blktests_build.sh
@@ -0,0 +1,13 @@
+set -x
+rm -rf rpmbuild_specs
+yum install -y rpm-build rpmdevtools make gcc gcc-c++ wget
+rm -rf /root/rpmbuild/*
+rpmdev-setuptree
+cd /root
+mkdir rpms
+cd /root/rpmbuild/SOURCES
+wget https://github.com/osandov/blktests/archive/master.zip
+cd /root
+git clone https://github.com/liangxiao1/rpmbuild_specs.git
+rpmbuild --undefine dist --define "debug_package %{nil}"  -ba /root/rpmbuild_specs/blktests.spec
+find /root/rpmbuild/RPMS -name "*.rpm" -exec mv {} rpms \;
---
+++ ltp_build.sh
@@ -0,0 +1,13 @@
+set -x
+rm -rf rpmbuild_specs
+yum install -y rpm-build rpmdevtools wget autoconf automake sysstat gcc quota git make
+rm -rf /root/rpmbuild/*
+rpmdev-setuptree
+cd /root
+mkdir rpms
+cd /root/rpmbuild/SOURCES
+wget https://github.com/linux-test-project/ltp/archive/master.zip
+cd /root
+git clone https://github.com/liangxiao1/rpmbuild_specs.git
+rpmbuild --undefine dist --define "debug_package %{nil}"  -ba /root/rpmbuild_specs/ltp.spec
+find /root/rpmbuild/RPMS -name "*.rpm" -exec mv {} rpms \;
